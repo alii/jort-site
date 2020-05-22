a5eb7d9a22598c38cdff759801180627adb0b1fd
Alistair Smith <alistairsmith01+git@gmail.com>
2020-05-22 20:44:58 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>
+        <i className="fas fa-bolt" /> jort
+      </h1>
+      <p>Screenshare is dead</p>
+    </div>
+  );
+};
+
+export default App;
